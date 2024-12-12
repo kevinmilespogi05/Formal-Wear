607ea5608e6fed605ea3700cf1b6c9dabb5a2a2a
acgbm <157551924+acgbm@users.noreply.github.com>
2024-12-12 22:17:38 +0800

    Updated Structure and Routes

--- formal-wear/src/app/app.routes.ts
+++ formal-wear/src/app/app.routes.ts
@@ -1,16 +1,45 @@
 import { Routes } from '@angular/router';
-import { LoginECommComponent } from './login/login.component';
-import { RegisterECommComponent } from './register/register.component';
-import { AuthGuard } from './guards/auth.guard';
-import { ProductComponent } from './product/product.component';
-import { CartComponent } from './cart/cart.component';
-import { AllProductsComponent } from './product-listing/product-listing.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { RentComponent } from './user/rent/rent.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { SidenavComponent as UserSidenavComponent } from './user/sidenav/sidenav.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { NewproductComponent } from './admin/newproduct/newproduct.component';
+import { ProductlistComponent } from './admin/productlist/productlist.component';
+import { AnalyticsComponent } from './admin/analytics/analytics.component';
+import { SidenavComponent as AdminSidenavComponent } from './admin/sidenav/sidenav.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'product-listing', component: AllProductsComponent, canActivate: [AuthGuard] },
-    { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
-    { path: 'product', component: ProductComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginECommComponent },
-    { path: 'register', component: RegisterECommComponent },
+  { path: '', redirectTo: '/rent', pathMatch: 'full' },
+  {
+    path: 'rent',
+    component: RentComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'profile',
+    component: UserSidenavComponent,
+    children: [
+      { path: '', component: ProfileComponent },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminSidenavComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'newproduct', component: NewproductComponent },
+      { path: 'productlist', component: ProductlistComponent },
+      { path: 'analytics', component: AnalyticsComponent },
+    ],
+  },
+  { path: '**', redirectTo: '/rent' },
 ];
